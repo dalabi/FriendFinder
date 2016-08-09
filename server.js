60eab6a7cb2fec8ed95495452b19254bb395fe46
Damola <d.labi@yahoo.com>
2016-08-09 16:28:54 -0400

    still trying

--- server.js
+++ server.js
@@ -6,3 +6,27 @@ var http = require('http');
 var app = express();
 var ANDRE = 3000;
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use(bodyParser.text());
+app.use(bodyParser.json({
+    type: 'application/vnd.api+json'
+}));
+
+var htmlRoutes = require('./app/routing/html-routes');
+var apiRoutes = require('./app/routing/api-routes');
+htmlRoutes.getBackground(app);
+htmlRoutes.getCss(app);
+htmlRoutes.getRoot(app);
+htmlRoutes.getSurvey(app);
+apiRoutes.postFriends(app);
+apiRoutes.getFriends(app);
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(ANDRE, function() {
+  console.log("App listening on PORT: "  + ANDRE);
+});
